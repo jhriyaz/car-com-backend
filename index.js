9a344483c73ca6c6fa36ebd1f2e64d6fd25f916f
jhriyaz <jhriyaz@gmail.com>
2023-10-19 14:47:58 +0600

    added add car post option

--- index.js
+++ index.js
@@ -28,12 +28,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const cars = client.db("CarCom").collection("cars");
+    
+  app.post('/cars',async(req,res)=>{
+    let car=req.body
+    const result = await myColl.insertOne(car);
+   res.send(result);
+  })
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+  
   }
 }
 run().catch(console.dir);
@@ -42,8 +55,9 @@ run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
-    res.send('server server')
+    res.send('welcome to Car-com')
 })
+
 app.listen(port,()=>{
     console.log('listening on port',port)
 })
